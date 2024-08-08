dfc9f29e4ddec6905de0ac7902ae985b4f1aa8f8
smknaake <bas.knaake@deltares.nl>
2024-08-08 14:27:34 +0200

    Initial commit

---
+++ lulucf/lasso.py
@@ -0,0 +1,7 @@
+class Lasso:
+    """
+    Containing definition of Lasso grid (25x25 m resolution?). This is the basic grid all
+    the calculations will be performed in and the results will be generated for.
+    
+    """
+    pass
