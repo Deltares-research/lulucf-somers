8c8ee6dafbf0c856cb709eac626cedfe69459702
smknaake <bas.knaake@deltares.nl>
2024-08-30 16:00:47 +0200

    Add rasterize_like

--- lulucf/utils.py
+++ lulucf/utils.py
@@ -7,6 +7,7 @@ import geopandas as gpd
 import numpy as np
 import pandas as pd
 import xarray as xr
+from rasterio import features
 from shapely.geometry import Polygon, box
 
 
@@ -89,3 +90,46 @@ def profile_function(func):
         return result
 
     return wrapper
+
+
+def rasterize_like(
+    shapefile: str | WindowsPath | gpd.GeoDataFrame,
+    attribute: str,
+    da: xr.DataArray,
+):
+    """
+    Rasterize a shapefile like an atmod Raster or into the 2D extent of a VoxelModel
+    object.
+
+    Parameters
+    ----------
+    shapefile : str | WindowsPath | gpd.GeoDataFrame
+        Input shapefile to rasterize. Can be a path to the shapefile or an in
+        memory GeoDataFrame.
+    attribute : str
+        Name of the attribute in the shapefile to rasterize.
+    da : xr.DataArray,
+        Atmod Raster or VoxelModel object to rasterize the shapefile like.
+    cellsize : int, optional
+        Cellsize of the output DataArray. The default is None, then the x and y
+        size will be derived from the input DataArray.
+
+    Returns
+    -------
+    xr.DataArray
+        DataArray of the rasterized shapefile.
+
+    """
+    if isinstance(shapefile, (str, WindowsPath)):
+        shapefile = gpd.read_file(shapefile)
+
+    shapes = ((geom, z) for z, geom in zip(shapefile[attribute], shapefile["geometry"]))
+
+    rasterized = features.rasterize(
+        shapes=shapes,
+        fill=np.nan,
+        out_shape=da.shape,
+        transform=da.rio.transform(),
+    )
+    rasterized = xr.DataArray(rasterized, coords=da.coords, dims=da.dims)
+    return rasterized
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,9 +1,11 @@
 import sqlite3
 
+import numpy as np
 import pytest
+from numpy.testing import assert_array_equal
 from shapely.geometry import Polygon
 
-from lulucf.utils import cell_as_geometry, create_connection
+from lulucf.utils import cell_as_geometry, create_connection, rasterize_like
 
 
 @pytest.fixture
@@ -40,3 +42,23 @@ def test_cell_as_geometry(cellsize, request):
 def test_create_connection(simple_soilmap_path):
     conn = create_connection(simple_soilmap_path)
     assert isinstance(conn, sqlite3.Connection)
+
+
+@pytest.mark.unittest
+def test_rasterize_like(lasso_grid, somers_parcels):
+    da = lasso_grid.dataarray()
+
+    raster = rasterize_like(somers_parcels, "parcel_id", da)
+
+    expected_values = [
+        [0, np.nan, 4, 4],
+        [1, 1, 4, 4],
+        [np.nan, np.nan, 2, np.nan],
+        [np.nan, 3, 3, np.nan],
+    ]
+
+    assert raster.shape == (4, 4)
+    assert raster.rio.bounds() == (0, 0, 4, 4)
+    assert raster.rio.resolution() == (1, -1)
+    assert raster.sizes == {'y': 4, 'x': 4}
+    assert_array_equal(raster.values, expected_values)
