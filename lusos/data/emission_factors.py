c7244052ddf61bf20fcc6b816745445a7fae1ef0
smknaake <bas.knaake@deltares.nl>
2025-06-10 10:14:00 +0200

    Replace emission factor constants for tested csv files downloaded with pooch

--- lusos/data/emission_factors.py
+++ lusos/data/emission_factors.py
@@ -13,10 +13,32 @@ REGISTRY.load_registry(resources.files("lusos.data") / "registry.txt")
 
 
 def ef_low_netherlands():
+    """
+    Emission factors for the western part (low) part of the Netherlands for different BGT
+    and soiltype combinations for different greenhouse gasses and flux directions (i.e. "out"
+    or "in"). Fetches the data from a CSV file on the lulucf-somers repository.
+
+    Returns
+    -------
+    pd.DataFrame
+        Emission factors with index of BGT-soiltype combinations.
+
+    """
     filename = REGISTRY.fetch("emission_factors_low_nl.csv")
     return pd.read_csv(filename, index_col="layer")
 
 
 def ef_high_netherlands():
+    """
+    Emission factors for the eastern part (high) part of the Netherlands for different BGT
+    and soiltype combinations for different greenhouse gasses and flux directions (i.e. "out"
+    or "in"). Fetches the data from a CSV file on the lulucf-somers repository.
+
+    Returns
+    -------
+    pd.DataFrame
+        Emission factors with index of BGT-soiltype combinations.
+
+    """
     filename = REGISTRY.fetch("emission_factors_high_nl.csv")
     return pd.read_csv(filename, index_col="layer")
