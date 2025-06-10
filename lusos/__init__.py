635f93c11d3231da9f4955b1ff815c1dc16ad229
smknaake <bas.knaake@deltares.nl>
2025-06-10 09:42:53 +0200

    Add pooch layout for emission factor csvs

--- lusos/__init__.py
+++ lusos/__init__.py
@@ -1,3 +1,4 @@
+from . import data
 from .area_statistics import calculate_model_flux
 from .coverage import bgt_soilmap_coverage
 from .emissions import calculate_somers_emissions
---
+++ lusos/data/emission_factors.py
@@ -0,0 +1,11 @@
+from importlib import resources
+
+import pandas as pd
+
+
+def ef_low_netherlands():
+    print(2)
+
+
+def ef_high_netherlands():
+    print(3)
