326f7970ee2710c7f93827cde3690d3ec0415d80
smknaake <bas.knaake@deltares.nl>
2025-06-10 09:57:34 +0200

    Update file hashes due to file error

--- lusos/data/emission_factors.py
+++ lusos/data/emission_factors.py
@@ -1,11 +1,22 @@
 from importlib import resources
 
 import pandas as pd
+import pooch
+
+REGISTRY = pooch.create(
+    path=pooch.os_cache("lusos"),
+    base_url="https://github.com/Deltares-research/lulucf-somers/raw/feature/pooch/data",
+    version=None,
+    env="LUSOS_DATA_DIR",
+)
+REGISTRY.load_registry(resources.files("lusos.data") / "registry.txt")
 
 
 def ef_low_netherlands():
-    print(2)
+    filename = REGISTRY.fetch("emission_factors_low_nl.csv")
+    return pd.read_csv(filename, index_col="layer")
 
 
 def ef_high_netherlands():
-    print(3)
+    filename = REGISTRY.fetch("emission_factors_high_nl.csv")
+    return pd.read_csv(filename, index_col="layer")
